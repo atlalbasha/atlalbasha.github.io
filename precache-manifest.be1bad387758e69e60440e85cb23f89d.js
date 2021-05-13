75d05e114cd25f47b8c31405d644218efcebf6bf
atlalbasha <atlal.basha@gmail.com>
2021-05-13 18:31:17 +0200

    init

---
+++ precache-manifest.be1bad387758e69e60440e85cb23f89d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f113651cd6dabc2be85f",
+    "url": "/css/app.6aca5246.css"
+  },
+  {
+    "revision": "2c4d786eaac9dba52b20f7c637d80dfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f113651cd6dabc2be85f",
+    "url": "/js/app.a2bfccd7.js"
+  },
+  {
+    "revision": "67998a7df0fd4286434d",
+    "url": "/js/chunk-vendors.bf76fb03.js"
+  },
+  {
+    "revision": "01012b5f6acf39a6d068c121bb7de726",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,36 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.be1bad387758e69e60440e85cb23f89d.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "vue-atlal"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/^http:\/\/www\.omdbapi\.com\/\?apikey=8e38a46a&t=.*$/, new workbox.strategies.NetworkFirst({ "networkTimeoutSeconds":5, plugins: [] }), 'GET');
